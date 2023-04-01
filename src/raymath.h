cc78be44bef54f3aa870cea5bcdc02b27b640048
Eric Barrett <neverloiter@gmail.com>
2023-04-01 12:34:13 -0230

    Keep Units in Radians
    Vector2RotateAround did not follow the convention of using radian units for unit circle operations. Fixed this

--- src/raymath.h
+++ src/raymath.h
@@ -443,8 +443,8 @@ RMAPI Vector2 Vector2RotateAround(Vector2 v, Vector2 origin, float angle)
 	Vector2 newPoint = {v.x - origin.x, v.y - origin.y};
 	Vector2 posInc;
 	// calculate values
-	float sinRotation = sinf(angle*DEG2RAD);
-	float cosRotation = cosf(angle*DEG2RAD);
+	float sinRotation = sinf(angle);
+	float cosRotation = cosf(angle);
 	posInc.x = newPoint.x * cosRotation - newPoint.y * sinRotation;
 	posInc.y = newPoint.x * sinRotation + newPoint.y * cosRotation;	
 	// translate back
